94147112fdfc2e03ba3460f55af934ccccc5ecf2
Davoud Razbari <drazbari@fb.com>
2021-05-04 20:38:29 +0100

    readme and cleanup

--- moveit.py
+++ moveit.py
@@ -6,30 +6,35 @@ from datetime import datetime
 button = mouse.Button
 mouse = mouse.Controller()
 
-key = keyboard.Key
-keyboard = keyboard.Controller()
+# I might add keyboard interaction. But here it is for now
+# key = keyboard.Key
+# keyboard = keyboard.Controller()
 
 
 
-count = 0
+count = 0 # to count how many times action is done!
 
 print('\n\nWelcome to MoveIt app!\n')
-def executeSomething():
+
+def moveIt():
     global count
     count = count + 1
+
     # Get time
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    currentPos = mouse.position
-    mouse.position = (2, 700)
-    time.sleep(0.1)
-    mouse.move(5, 5)
-    # time.sleep(1)
-    time.sleep(0.1)
-    mouse.click(button.right, 1)
 
-    time.sleep(0.1)
-    mouse.position = (int(currentPos[0]), int(currentPos[1]))
+    currentPos = mouse.position # save current position of mouse!
+    mouse.position = (2, 700) # move it to the left of the screen somewhere in the middle!
+
+    time.sleep(0.1) # Wait a bit...
+    mouse.move(5, 5)
+
+    time.sleep(0.1) # Wait again...
+    mouse.click(button.right, 1) # make a right click!
+
+    time.sleep(0.1) # Wait...
+    mouse.position = (int(currentPos[0]), int(currentPos[1])) #move mouse back to where it was!
     print("Count:", count, "Mouse moved at:", current_time)
 
     # # Keyboard action
@@ -41,4 +46,4 @@ def executeSomething():
     time.sleep(120)
 
 while True:
-    executeSomething()
+    moveIt()
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Move It!
+> A **Python** app that moves the mouse every two minutes and make a mouse action. This is my first Python app!
+
+Feel free to fork it and [let me](mailto:starchcode@gmail.com) know if you have any suggestions!
+
+&nbsp;
+
+## Table of contents
+
+  - [General info](#general-info)
+    - [Library Used](#library-used)
+  - [Status](#status)
+  - [Contact](#contact)
+
+## General info
+
+This app is created in order to avoid the screen from going to sleep.
+
+### Library Used
+* pynput: [see documentation](https://pynput.readthedocs.io/en/latest/index.html)
+
+To-do list:
+
+- [x] ~~Move mouse to the side of the screen and click!~~
+- [ ] Make a sound when click is done!
+
+## Status
+
+![progress](https://img.shields.io/badge/Progress-80%_Complete-green?style=for-the-badge)
+
+## Contact
+
+Created by Dave Raspberry - feel free to [contact me](mailto:starchcode@gmail.com) If you have any questions. I would be glad to help 🥸
